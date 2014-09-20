4d9b6dc22fd19cb50f0fabec2d618f98a082e6c2
Davmira <davmurat@msn.com>
2014-09-20 18:21:15 -0400

    Création du servlet subscribe

---
+++ src/ca/etsmtl/log210/Controller/Subscribe.java
@@ -0,0 +1,48 @@
+package ca.etsmtl.log210.Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ca.etsmtl.log210.Beans.UserAccountBean;
+import ca.etsmtl.log210.DAO.DAOFactory;
+import ca.etsmtl.log210.DAO.UserAccountDao;
+
+public class Subscribe extends HttpServlet {
+
+	public static final String CONF_DAO_FACTORY = "daofactory";
+	// The instance of UserAccountDao who give us the possibility to execute
+	// requests to the DB about userAccount
+	private UserAccountDao userAccountDao;
+
+	/**
+	 * Method who is executed the fist time that the servlet is create. Here we
+	 * get the connection to the DB throw UserAccountDao class. We must get the
+	 * connection just once if we don't want to have a too many connection error
+	 * in MySql.
+	 */
+	public void init() throws ServletException {
+		this.userAccountDao = ((DAOFactory) getServletContext().getAttribute(
+				CONF_DAO_FACTORY)).getUserAccountDao();
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//On créé un userAccountBean pour stocker les valeurs rentrées dans le formulaire.
+		
+		UserAccountBean newUser = new UserAccountBean();
+		
+		//On set newUser les valeurs rentrées dans le formulaire ok nickel.
+		
+		newUser.setName(request.getParameter("name"));
+		newUser.setFirstName(request.getParameter("firstname"));
+		newUser.setEmail(request.getParameter("email"));
+		newUser.setPassWord(request.getParameter("password1"));
+		newUser.setBirthdayDate(request.getParameter("birthday"));
+		newUser.setHomeAddress(request.getParameter("adress"));
+		newUser.setPhoneNumber(request.getParameter("phone"));
+	}
+}
