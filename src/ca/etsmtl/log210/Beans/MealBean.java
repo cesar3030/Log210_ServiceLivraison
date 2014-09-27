c36de36d403d80805d04f5ac5e89bc90a3fee1a3
Davmira <davmurat@msn.com>
2014-09-27 14:59:32 -0400

    Correctifs

--- src/ca/etsmtl/log210/Beans/MealBean.java
+++ src/ca/etsmtl/log210/Beans/MealBean.java
@@ -1,72 +1,91 @@
 package ca.etsmtl.log210.Beans;
 
-public class PlatBean {
-	
-	private  int idPlat;
+/**
+ * Bean qui represente un plat dans la BDD
+ * @author David
+ *
+ */
+public class MealBean {
+
+	private int idPlat;
 	private int idMenu;
 	private int price;
 	private String name;
 	private String description;
+
 	/**
 	 * @return the description
 	 */
 	public String getDescription() {
 		return description;
 	}
+
 	/**
-	 * @param description the description to set
+	 * @param description
+	 *            the description to set
 	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
+
 	/**
-	 * @param name the name to set
+	 * @param name
+	 *            the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	/**
 	 * @return the price
 	 */
 	public int getPrice() {
 		return price;
 	}
+
 	/**
-	 * @param price the price to set
+	 * @param price
+	 *            the price to set
 	 */
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	/**
 	 * @return the idMenu
 	 */
 	public int getIdMenu() {
 		return idMenu;
 	}
+
 	/**
-	 * @param idMenu the idMenu to set
+	 * @param idMenu
+	 *            the idMenu to set
 	 */
 	public void setIdMenu(int idMenu) {
 		this.idMenu = idMenu;
 	}
+
 	/**
 	 * @return the idPlat
 	 */
 	public int getIdPlat() {
 		return idPlat;
 	}
+
 	/**
-	 * @param idPlat the idPlat to set
+	 * @param idPlat
+	 *            the idPlat to set
 	 */
 	public void setIdPlat(int idPlat) {
 		this.idPlat = idPlat;
 	}
-	
 
 }
