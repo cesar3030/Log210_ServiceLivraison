b7d1cdae679ab9992ae00c0da52e928f4384024c
Idriss Aissou <i7.aissou@gmail.com>
2014-09-27 22:27:29 -0400

    correction

--- src/ca/etsmtl/log210/DAO/RestaurantDaoImpl.java
+++ src/ca/etsmtl/log210/DAO/RestaurantDaoImpl.java
@@ -3,10 +3,26 @@
  */
 package ca.etsmtl.log210.DAO;
 
+import java.util.ArrayList;
+
+import ca.etsmtl.log210.Beans.RestaurantBean;
+
 /**
  * @author Iron_Cesar
  *
  */
 public class RestaurantDaoImpl implements RestaurantDao {
+
+	@Override
+	public ArrayList<RestaurantBean> getActiveRestaurants() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<RestaurantBean> getInnactiveRestaurants() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
