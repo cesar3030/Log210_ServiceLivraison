85c540fedd0d9e22156e2cc281ce8032ea6b9e70
Davmira <davmurat@msn.com>
2014-10-01 19:19:18 -0400

    Implémentantion de MealDao et MealDaoImpl, ajout de la méthode pour traiter la requete

--- src/ca/etsmtl/log210/DAO/MealDaoImpl.java
+++ src/ca/etsmtl/log210/DAO/MealDaoImpl.java
@@ -1,5 +1,11 @@
 package ca.etsmtl.log210.DAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import ca.etsmtl.log210.Beans.MealBean;
+import ca.etsmtl.log210.Beans.MenuBean;
+
 public class MealDaoImpl  implements MealDao{
 	
 	private DAOFactory daoFactory;
@@ -9,4 +15,18 @@ public class MealDaoImpl  implements MealDao{
 		this.daoFactory = daoFactory;		
 	}
 
+	
+	//Permet de reformer un Bean Meal de la BD pour pourvoir travailler avec les donn�es provenant de la requete SQL
+	private MealBean mapMealBean(ResultSet resultSet) throws SQLException 
+	{
+		MealBean meal = new MealBean();
+		
+		meal.setIdPlat(resultSet.getInt("PLA_idPlat"));
+		meal.setIdMenu(resultSet.getInt("PLA_idMenu"));
+		meal.setPrice(resultSet.getInt("PLA_price"));
+		meal.setName(resultSet.getString("PLA_nom"));
+		meal.setDescription(resultSet.getString("PLA_description"));
+				
+		return meal;
+	}
 }
