185190517049cb8fbd7de55c8fafc7c3d9bc8a3c
Davmira <davmurat@msn.com>
2014-10-01 19:39:06 -0400

    Méthode d'ajout en BDD complété ! RÉfactor d'une variable et commentaires

--- src/ca/etsmtl/log210/DAO/MealDao.java
+++ src/ca/etsmtl/log210/DAO/MealDao.java
@@ -1,5 +1,12 @@
 package ca.etsmtl.log210.DAO;
 
+import ca.etsmtl.log210.Beans.MealBean;
+import ca.etsmtl.log210.Beans.MenuBean;
+
 public interface MealDao {
+	
+
+	public boolean addNewMeal(MealBean mealRecept);
+
 
 }
