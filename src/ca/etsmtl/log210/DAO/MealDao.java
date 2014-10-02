97451ebdb52ed6fb8060f60d2da00b05046b10a3
Davmira <davmurat@msn.com>
2014-10-02 12:43:05 -0400

    Ajout de la possibilite de liste des plats à partir d'un idMenu

--- src/ca/etsmtl/log210/DAO/MealDao.java
+++ src/ca/etsmtl/log210/DAO/MealDao.java
@@ -1,5 +1,7 @@
 package ca.etsmtl.log210.DAO;
 
+import java.util.ArrayList;
+
 import ca.etsmtl.log210.Beans.MealBean;
 import ca.etsmtl.log210.Beans.MenuBean;
 
@@ -8,6 +10,8 @@ public interface MealDao {
 
 	public boolean addNewMeal(MealBean mealRecept);
 	public boolean deleteNewMeal(MealBean mealRecept);
+	
+	ArrayList<MealBean> showAllMealFromMenu(int idMenu);
 
 
 }
