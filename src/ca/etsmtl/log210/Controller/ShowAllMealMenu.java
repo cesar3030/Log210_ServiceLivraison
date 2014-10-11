333903beafecb75372cb1a6b44d7379bf66ded5d
Davmira <davmurat@msn.com>
2014-10-11 14:02:38 -0400

    AJout d'un menu fonctionnel

--- src/ca/etsmtl/log210/Controller/ShowAllMealMenu.java
+++ src/ca/etsmtl/log210/Controller/ShowAllMealMenu.java
@@ -18,18 +18,18 @@ public class ShowAllMealMenu extends HttpServlet {
 	/**
 	 * 
 	 */
-	
+
 	private static final long serialVersionUID = 6561975988106199732L;
 	public static final String CONF_DAO_FACTORY = "daofactory";
 	public static final String MENU_MANAGEMENT_ACCESS = "/Restrict/Restaurateur/ShowAllMealMenu.jsp";
 	public static final String MEAL_MENU_ATTRIBUTE = "mealMenuList";
 	public static final String MEAL_MENU_TITLE_ATTRIBUTE = "mealMenu";
 	public static final String ID_MENU_SESSION = "idMenuSession";
+	public static final String MENU_NAME_TITRE = "menuTitleName";
+
+	public String MENU_NAME = "menuName";
 	public int ID_MENU;
 	public int ID_RESTAURANT;
-
-	public String MENU_NAME_TITRE = "menuTitleName";
-	public String MENU_NAME = "menuName";
 
 	// Instance de menu qui va nous permettre de faire des requetes sur la BD
 	private MealDao mealDao;
@@ -55,18 +55,33 @@ public class ShowAllMealMenu extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		 /* Récupération de la session depuis la requête */
-        HttpSession session = request.getSession();
-        
-		ID_MENU = Integer.parseInt(request.getParameter("idMenu"));
-		session.setAttribute(ID_MENU_SESSION,request.getParameter("idMenu"));
-		
+		/* Récupération de la session depuis la requête */
+		System.out.println("-----DÉBUT SHOW ALL MEAL MENU DÉBUT------");
+		HttpSession session = request.getSession();
+
+		if (request.getParameter("idMenu") != null) {
+			ID_MENU = Integer.parseInt(request.getParameter("idMenu"));
+			session.setAttribute(ID_MENU_SESSION,
+					request.getParameter("idMenu"));
+		} else {
+			ID_MENU = Integer.parseInt((String) session
+					.getAttribute("idMenuSession"));
+			session.setAttribute(ID_MENU_SESSION,
+					session.getAttribute("idMenuSession"));
+		}
+
 		MENU_NAME = request.getParameter("name");
-		
-		ID_RESTAURANT = Integer.parseInt(request.getParameter("idRestaurant"));
+
+		if (request.getParameter("idRestaurant") != null) {
+			ID_RESTAURANT = Integer.parseInt(request
+					.getParameter("idRestaurant"));
+			session.setAttribute("idRestaurant", ID_RESTAURANT);
+		}
+
 		System.out.println("voici l'id du menu : " + ID_MENU);
 		System.out.println("voici le nom du menu : " + MENU_NAME);
-		System.out.println("voici l'id du restaurant : " + request.getParameter("idRestaurant"));
+		System.out.println("voici l'id du restaurant : "
+				+ request.getParameter("idRestaurant"));
 		// Creation des liste de donnes de requete
 		ArrayList<MealBean> mealList;
 
@@ -77,10 +92,12 @@ public class ShowAllMealMenu extends HttpServlet {
 		request.setAttribute(MEAL_MENU_ATTRIBUTE, mealList);
 		request.setAttribute(MENU_NAME_TITRE, MENU_NAME);
 		request.setAttribute("idRestaurant", ID_RESTAURANT);
+		request.setAttribute(ID_MENU_SESSION, ID_MENU);
 
 		// On renvoie la requete de reponse au bon endroit du restrict
 		this.getServletContext().getRequestDispatcher(MENU_MANAGEMENT_ACCESS)
 				.forward(request, response);
+		System.out.println("-----FIN SHOW ALL MEAL MENU FIN------");
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
